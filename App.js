7d2a2a2e3f66063d0d73ee44513a02f9c2a93769
Mozhde Nourani <mozhde@globalworkandtravel.com>
2022-11-01 17:44:05 +0330

    Implement push notification Quit, Background, Foreground

--- App.js
+++ App.js
@@ -1,9 +1,40 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
 import {SafeAreaView} from 'react-native';
 import {WebView} from 'react-native-webview';
+import messaging from '@react-native-firebase/messaging';
 
+async function requestUserPermission() {
+  const authStatus = await messaging().requestPermission();
+  const enabled =
+    authStatus === messaging.AuthorizationStatus.AUTHORIZED ||
+    authStatus === messaging.AuthorizationStatus.PROVISIONAL;
+  if (enabled) {
+    // console.log('FCM Authorization status:', authStatus);
+  }
+}
 const App = () => {
+  useEffect(() => {
+    const getToken = async () => {
+      await messaging().registerDeviceForRemoteMessages();
+      const token = await messaging().getToken();
+      console.log(token);
+    };
+    getToken();
+  }, []);
+
+  messaging().setBackgroundMessageHandler(async remoteMessage => {
+    console.log('A new FCM message ddddddd!', JSON.stringify(remoteMessage));
+  });
+  useEffect(() => {
+    const unsubscribe = messaging().onMessage(async remoteMessage => {
+      console.log('A new FCM message arrived!', JSON.stringify(remoteMessage));
+    });
+    requestUserPermission();
+
+    return unsubscribe;
+  }, []);
+
   return (
     <SafeAreaView style={{flex: 1}}>
       <WebView
